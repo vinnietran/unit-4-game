d8bb1695bd35f2a2dad2a3dbb6c97728ab2e877b
VinTran <tranquillo.vinnie@gmail.com>
2019-02-12 22:27:20 -0500

    Got it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,14 @@
 
 var randomNumber;
 var userRandNumber;
+var RandNumber = [];
 var userScore;
 var wins = 0;
 var losses = 0;
-var images = [1,2,3,4]; 
+
+var images = ['assets/images/greenCrystal.jpg', 'assets/images/yellowCrystal.jpeg',
+                 'assets/images/redCrystal.jpg', 'assets/images/blueCrystal.jpg']
+
 
 
 
@@ -13,33 +17,49 @@ $(document).ready(startGame)
 
 //Generates random numbers at start of game
 function startGame() {
-    randomNumber = [Math.floor(Math.random() * (120 - 19) + 19)];
-    console.log(randomNumber);
-    $("#rnd").append(randomNumber);
+  randomNumber = [Math.floor(Math.random() * (120 - 19) + 19)];
+  console.log(randomNumber);
+  $("#rnd").append(randomNumber);
 
-    //generates numbers for the images 
-    for (i=0; i<images.length; i++){
-        userRandNumber = [Math.floor(Math.random() * (12 - 1)-0)];
-        console.log(userRandNumber);
 
-        var crystals = "red-crystal";
-
+  //generates numbers for the images 
+  for (i = 0; i < images.length; i++) {
+    userRandNumber = [Math.floor(Math.random() * (12 - 1) - 0)];
+    RandNumber.push(userRandNumber);
+    //console.log(userRandNumber);
     
-        //crystals.attr("data-crystalValue", userRandNumber[i]);
-    
-      // $("#red-crystal").append(userRandNumber);
 
-        //console.log(userRandNumber);
-        }
+  }
+  console.log(RandNumber);
 
-    
+
+  for (i = 0; i < images.length; i++) {
+
+    var imageCrystal = $("<img>");
+    // First each crystal will be given the class ".crystal-image".
+    // This will allow the CSS to take effect.
+    imageCrystal.addClass("col-md-1");
+
+    // Each imageCrystal will be given a src link to the crystal image
+    imageCrystal.attr("src", images[i]);
+    console.log(imageCrystal);
+
+    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // This data attribute will be set equal to the array value.
+    imageCrystal.attr("data-crystalvalue", RandNumber[i]);
+
+    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    $("#images").append(imageCrystal);
+  }
+
+
 
 }
 
-$("#red-crystal").on("click", function() {
-  var clickValue = $("data-crystalValue");
-  console.log(clickValue);
+//$("#red-crystal").on("click", function () {
+  //var clickValue = $("data-crystalValue");
+ // console.log("fuck");
 
-    
-});
+
+//});
 
